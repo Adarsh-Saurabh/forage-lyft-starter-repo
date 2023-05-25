7109473fabe0a439b26d8146bbf5b1803dba8f17
Adarsh Saurabh <75805225+Adarsh-Saurabh@users.noreply.github.com>
2023-05-26 02:35:06 +0530

    adding something something

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,15 +1,16 @@
 from abc import ABC
-
+from engine import Engine
 from car import Car
 
-
-class SternmanEngine(Car, ABC):
-    def __init__(self, last_service_date, warning_light_is_on):
-        super().__init__(last_service_date)
+class SternmanEngine(Engine):
+    def __init__(self, last_service_mileage, current_mileage, service_threshold, warning_light_is_on):
+        super().__init__(last_service_mileage, current_mileage, service_threshold)
         self.warning_light_is_on = warning_light_is_on
 
-    def engine_should_be_serviced(self):
-        if self.warning_light_is_on:
-            return True
-        else:
-            return False
+# Instantiate the SternmanEngine
+last_service_mileage = 10000
+current_mileage = 12000
+service_threshold = 2000
+warning_light_is_on = True
+
+engine = SternmanEngine(last_service_mileage, current_mileage, service_threshold, warning_light_is_on)
